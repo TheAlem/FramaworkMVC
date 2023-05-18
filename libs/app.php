dab62886b7f62493c87ad882822d3392409301ea
Jose Alem <josealem03@gmail.com>
2023-05-18 11:34:50 -0400

    Hecho la clase, pero aun no termina

--- libs/app.php
+++ libs/app.php
@@ -3,6 +3,10 @@ class App{
     function __construct()
     {
         echo " <p>Mi aplicacion MVC de WEB II";
+        //leogica en la url
+        $url = $_GET['url'];
+        echo "<br>";
+        echo $url;
     }
 }
 
