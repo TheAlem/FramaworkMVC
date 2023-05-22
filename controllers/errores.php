0f99f62d17b8a0e89e95ffb513f5d75e4b47a7e7
Jose Alem <josealem03@gmail.com>
2023-05-22 11:36:56 -0400

    error

---
+++ controllers/errores.php
@@ -0,0 +1,8 @@
+<?php
+class Errores{
+    function __construct()
+    {
+        echo "<p style= 'color:red'>No existe el recursor </p>";
+    }
+}
+?>
--- libs/app.php
+++ libs/app.php
@@ -2,20 +2,30 @@
 class App{
     function __construct()
     {
-        echo " <p>Mi aplicacion MVC de WEB II";
+        echo " <p>Mi aplicacion en WEB II";
         //leogica en la url
         $url = $_GET['url'];
-        echo "<br>";    
+        echo "<br>";
         echo $url;
-        $url = explode("/", $url);
-        echo"<br>";
+        $url=explode("/", $url);
+        echo "<br>";
         echo $url[0];
         echo "<br>";
+
+        //llamamos al controlador
+        $ruta="controllers/".$url[0].".php";
+        echo $ruta;
         echo "<br>";
-        //Llamamos al controlador
-        $ruta = "controllers/".$url[0].".php";
-        require_once $ruta;
-        $c = new $url[0];
+        if (file_exists($ruta)){
+            require_once $ruta;
+            $c = new $url[0];
+        }else{
+            require_once "controllers/errores.php";
+            $e= new Errores();
+        }
+        //echo "Ruta:".$ruta;
+
+
     }
 }
 
